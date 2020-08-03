4546d74252e0e78e193e63f8d31c70212944dfa1
Hans Larsen <hans@larsen.online>
2020-08-03 09:47:21 -0700

    adding random port

--- ic-agent/tests/ic_ref/utils.rs
+++ ic-agent/tests/ic_ref/utils.rs
@@ -2,8 +2,11 @@ use ic_agent::{Agent, AgentConfig};
 
 pub async fn create_agent() -> Result<Agent, String>
 {
+    let port_env = std::env::var("IC_REF_PORT").expect("Need to specify the IC_REF_PORT environment variable.");
+    let port = port_env.parse::<u32>().expect("Could not parse the IC_REF_PORT environment variable as an integer.");
+
     Ok(ic_agent::Agent::new(AgentConfig {
-        url: "http://127.0.0.1:8001",
+        url: &format!("http://127.0.0.1:{}", port),
         ..AgentConfig::default()
     })?)
 }
