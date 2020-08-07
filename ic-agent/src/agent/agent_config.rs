0fc6f89f70743fa1e227587f02909125855e4a9f
Hans <hans.larsen@dfinity.org>
2020-08-07 08:10:25 -0700

    feat: refactor the API to not have dependent types crossing the API layer (#9)
    Previously, we were exposing dependencies through the API layer (e.g. with the `AgentRequestExecutor`). This is a big no no, as those types need to come from crates that are the same version and that cannot be enforced properly outside of a library.
    
    The only reason we needed the Executor in DFX was for password management. So instead there is now new APIs that deal specifically with the features we are trying to enable (like HTTP Auth passwords).
    
    This is a breaking change but since nobody depends on this repo just yet (outside of DFX), it works fine.

--- ic-agent/src/agent/agent_config.rs
+++ ic-agent/src/agent/agent_config.rs
@@ -1,30 +1,29 @@
 use crate::identity::dummy::DummyIdentity;
 use crate::identity::Identity;
-use crate::{AgentError, NonceFactory};
-use async_trait::async_trait;
+use crate::NonceFactory;
 
-#[async_trait]
-pub trait AgentRequestExecutor: Sync + Send {
-    async fn execute(&self, request: reqwest::Request) -> Result<reqwest::Response, AgentError>;
-}
+/// Implemented by the Agent environment to cache and update an HTTP Auth password.
+/// It returns a tuple of `(username, password)`.
+pub trait PasswordManager {
+    /// Retrieve the cached value for a user. If no cache value exists for this URL,
+    /// the manager can return [`None`].
+    fn cached(&self, url: &str) -> Result<Option<(String, String)>, String>;
 
-pub struct DefaultAgentClient {
-    client: reqwest::Client,
-}
-
-#[async_trait]
-impl AgentRequestExecutor for DefaultAgentClient {
-    async fn execute(&self, request: reqwest::Request) -> Result<reqwest::Response, AgentError> {
-        self.client.execute(request).await.map_err(AgentError::from)
-    }
+    /// A call to the replica failed, so in order to succeed a username and password
+    /// is required. If one cannot be provided (for example, there's no terminal),
+    /// this should return an error.
+    /// If the username and password provided by this method does not work (the next
+    /// request still returns UNAUTHORIZED), this will be called and the request will
+    /// be retried in a loop.
+    fn required(&self, url: &str) -> Result<(String, String), String>;
 }
 
 pub struct AgentConfig<'a> {
     pub url: &'a str,
     pub nonce_factory: NonceFactory,
     pub identity: Box<dyn Identity>,
-    pub request_executor: Box<dyn AgentRequestExecutor>,
     pub default_waiter: delay::Delay,
+    pub password_manager: Option<Box<dyn PasswordManager>>,
 }
 
 impl Default for AgentConfig<'_> {
@@ -34,12 +33,8 @@ impl Default for AgentConfig<'_> {
             url: "-",
             nonce_factory: NonceFactory::random(),
             identity: Box::new(DummyIdentity {}),
-            request_executor: Box::new(DefaultAgentClient {
-                client: reqwest::Client::builder()
-                    .build()
-                    .expect("Could not create HTTP client."),
-            }),
             default_waiter: delay::Delay::instant(),
+            password_manager: None,
         }
     }
 }
