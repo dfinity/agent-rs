59d63b8e701f0d99a26c1cb508bc0187c4983e56
Hans <hans.larsen@dfinity.org>
2020-08-20 16:13:07 -0700

    feat: add support for reading a pem file in BasicIdentity (#18)
    * feat: add support for reading a pem file in BasicIdentity
    
    * basic documentation for the BasicIdentity

--- ic-agent/src/identity/basic.rs
+++ ic-agent/src/identity/basic.rs
@@ -1,11 +1,44 @@
 use crate::{Blob, Identity, Principal, Signature};
 use ring::signature::{Ed25519KeyPair, KeyPair};
+use std::path::Path;
+use thiserror::Error;
 
+/// An error happened while reading a PEM file to create a BasicIdentity.
+#[derive(Error, Debug)]
+pub enum PemError {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+
+    #[error("An error occured while reading the file: {0}")]
+    PemError(#[from] pem::PemError),
+
+    #[error("A key was rejected by Ring: {0}")]
+    KeyRejected(#[from] ring::error::KeyRejected),
+}
+
+/// A Basic Identity which sign using an ED25519 key pair.
 pub struct BasicIdentity {
     key_pair: Ed25519KeyPair,
 }
 
 impl BasicIdentity {
+    /// Create a BasicIdentity from reading a PEM file at the path.
+    pub fn from_pem_file<P: AsRef<Path>>(file_path: P) -> Result<Self, PemError> {
+        Self::from_pem(std::fs::File::open(file_path)?)
+    }
+
+    /// Create a BasicIdentity from reading a PEM File from a Reader.
+    pub fn from_pem<R: std::io::Read>(pem_reader: R) -> Result<Self, PemError> {
+        let bytes: Vec<u8> = pem_reader
+            .bytes()
+            .collect::<Result<Vec<u8>, std::io::Error>>()?;
+
+        Ok(Self {
+            key_pair: Ed25519KeyPair::from_pkcs8(pem::parse(&bytes)?.contents.as_slice())?,
+        })
+    }
+
+    /// Create a BasicIdentity from a KeyPair from the ring crate.
     pub fn from_key_pair(key_pair: Ed25519KeyPair) -> Self {
         Self { key_pair }
     }
