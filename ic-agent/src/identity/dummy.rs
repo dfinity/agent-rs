a1d63ef5381b7cac71a5f9f1fadee69ba9d1578c
Eric Swanson <eric.swanson@dfinity.org>
2020-10-15 14:21:45 -0700

    44-byte dummy DER encoded public key

--- ic-agent/src/identity/dummy.rs
+++ ic-agent/src/identity/dummy.rs
@@ -13,7 +13,7 @@ impl Identity for DummyIdentity {
         Ok(Signature {
             signature: vec![1; 32],
             public_key: vec![2; 32],
-            der_encoded_public_key: vec![3; 32],
+            der_encoded_public_key: vec![3; 44],
         })
     }
 }
