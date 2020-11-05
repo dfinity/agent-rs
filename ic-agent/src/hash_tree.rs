45ceefb4d7aa7dee79b47599e20fffbe0f2df7fa
Eric Swanson <eric.swanson@dfinity.org>
2020-11-05 12:02:07 -0800

    TreeHash deserialization

--- ic-agent/src/hash_tree.rs
+++ ic-agent/src/hash_tree.rs
@@ -329,3 +329,104 @@ impl TryFrom<HashTree> for Simple<Vec<u8>> {
         })
     }
 }
+
+impl<'de> serde::de::Deserialize<'de> for HashTree {
+    fn deserialize<D>(deserializer: D) -> Result<HashTree, D::Error>
+        where
+            D: serde::de::Deserializer<'de>,
+    {
+        use serde::de::{self, IgnoredAny, SeqAccess, Visitor};
+
+        struct SeqVisitor;
+
+        impl<'de> Visitor<'de> for SeqVisitor {
+            type Value = HashTree;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str(
+                    "HashTree encoded as a sequence of the form \
+                     hash-tree ::= [0] | [1 hash-tree hash-tree] | [2 bytes hash-tree] | [3 bytes] | [4 hash]",
+                )
+            }
+
+            fn visit_seq<V>(self, mut seq: V) -> Result<Self::Value, V::Error>
+                where
+                    V: SeqAccess<'de>,
+            {
+                let tag: u8 = seq
+                    .next_element()?
+                    .ok_or_else(|| de::Error::invalid_length(0, &self))?;
+
+                match tag {
+                    0 => {
+                        if let Some(IgnoredAny) = seq.next_element()? {
+                            return Err(de::Error::invalid_length(2, &self));
+                        }
+
+                        Ok(HashTree::Empty)
+                    }
+                    1 => {
+                        let left: HashTree = seq
+                            .next_element()?
+                            .ok_or_else(|| de::Error::invalid_length(1, &self))?;
+                        let right: HashTree = seq
+                            .next_element()?
+                            .ok_or_else(|| de::Error::invalid_length(2, &self))?;
+
+                        if let Some(IgnoredAny) = seq.next_element()? {
+                            return Err(de::Error::invalid_length(4, &self));
+                        }
+
+                        Ok(HashTree::Fork(Box::new((left, right))))
+                    }
+                    2 => {
+                        let label: Label = seq
+                            .next_element()?
+                            .ok_or_else(|| de::Error::invalid_length(1, &self))?;
+                        let subtree: HashTree = seq
+                            .next_element()?
+                            .ok_or_else(|| de::Error::invalid_length(2, &self))?;
+
+                        if let Some(IgnoredAny) = seq.next_element()? {
+                            return Err(de::Error::invalid_length(4, &self));
+                        }
+
+                        Ok(HashTree::Labeled(label, Box::new(subtree)))
+                    }
+                    3 => {
+                        let bytes: serde_bytes::ByteBuf = seq
+                            .next_element()?
+                            .ok_or_else(|| de::Error::invalid_length(1, &self))?;
+
+                        if let Some(IgnoredAny) = seq.next_element()? {
+                            return Err(de::Error::invalid_length(3, &self));
+                        }
+
+                        Ok(HashTree::Leaf(bytes.into_vec()))
+                    }
+                    4 => {
+                        let digest_bytes: serde_bytes::ByteBuf = seq
+                            .next_element()?
+                            .ok_or_else(|| de::Error::invalid_length(1, &self))?;
+
+                        if let Some(IgnoredAny) = seq.next_element()? {
+                            return Err(de::Error::invalid_length(3, &self));
+                        }
+
+                        let digest = Digest::try_from(digest_bytes.into_vec()).map_err(|err| {
+                            de::Error::invalid_length(err.len(), &"Expected digest blob")
+                        })?;
+
+                        Ok(HashTree::Pruned(digest))
+                    }
+                    _ => Err(de::Error::custom(format!(
+                        "unknown tag: {}, expected the tag to be one of {{0, 1, 2, 3, 4}}",
+                        tag
+                    ))),
+                }
+            }
+        }
+
+        deserializer.deserialize_seq(SeqVisitor)
+    }
+}
