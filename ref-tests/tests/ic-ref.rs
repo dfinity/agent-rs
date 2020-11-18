ec0fe2dd4baac0816b9e0f09f6238a195e20f7a3
Eric Swanson <eric.swanson@dfinity.org>
2020-11-18 15:56:04 -0800

    chore: upgrade to ic-ref 0.14

--- ref-tests/tests/ic-ref.rs
+++ ref-tests/tests/ic-ref.rs
@@ -13,7 +13,7 @@
 use ref_tests::universal_canister;
 use ref_tests::with_agent;
 
-const EXPECTED_IC_API_VERSION: &str = "0.13.2";
+const EXPECTED_IC_API_VERSION: &str = "0.14.0";
 
 #[ignore]
 #[test]
