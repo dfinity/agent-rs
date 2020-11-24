f1c39e255672bc910ce37ea1d476f392a455c881
Hans Larsen <hans@larsen.online>
2020-11-24 11:05:53 -0800

    fix other match

--- ic-agent/src/agent/agent_error.rs
+++ ic-agent/src/agent/agent_error.rs
@@ -107,7 +107,7 @@ impl HttpErrorPayload {
                     "Http Error: status {}, content type {:?}, content: {}",
                     StatusCode::from_u16(*status)
                         .map_or_else(|_| format!("{}", status), |code| format!("{}", code)),
-                    content_type.as_ref().unwrap(),
+                    content_type.clone().unwrap_or("".to_string()),
                     String::from_utf8(content.to_vec()).unwrap_or_else(|from_utf8_err| format!(
                         "(unable to decode content: {:#?})",
                         from_utf8_err
