2225718e5c85484500c91269a8d5f41c222bfd0d
Eric Swanson <eric.swanson@dfinity.org>
2021-01-14 17:04:14 -0800

    format

--- ref-tests/src/utils.rs
+++ ref-tests/src/utils.rs
@@ -2,13 +2,13 @@ use delay::Delay;
 use ic_agent::export::Principal;
 use ic_agent::identity::BasicIdentity;
 use ic_agent::{Agent, Identity};
+use ic_identity_hsm::HardwareIdentity;
 use ic_utils::call::AsyncCall;
 use ic_utils::interfaces::ManagementCanister;
 use ring::signature::Ed25519KeyPair;
 use std::error::Error;
 use std::future::Future;
 use std::path::Path;
-use ic_identity_hsm::HardwareIdentity;
 
 pub fn create_waiter() -> Delay {
     Delay::builder()
@@ -31,15 +31,14 @@ pub async fn create_hsm_identity() -> Result<HardwareIdentity, String> {
     let path = std::env::var("HSM_PKCS11_LIBRARY_PATH")
         .expect("Need to specify the HSM_PKCS11_LIBRARY_PATH environment variable");
     let slot = 0;
-    let key = std::env::var("HSM_KEY_ID")
-        .expect("Need to specify the HSM_KEY_ID environment variable");
+    let key =
+        std::env::var("HSM_KEY_ID").expect("Need to specify the HSM_KEY_ID environment variable");
     HardwareIdentity::new(path, slot, &key, get_hsm_pin)
-        .map_err(|e|format!("Unable to create hw identity: {}", e))
+        .map_err(|e| format!("Unable to create hw identity: {}", e))
 }
 
 fn get_hsm_pin() -> Result<String, String> {
-    std::env::var("HSM_PIN")
-        .map_err(|_| "There is no HSM_PIN environment variable.".to_string())
+    std::env::var("HSM_PIN").map_err(|_| "There is no HSM_PIN environment variable.".to_string())
 }
 
 pub async fn create_basic_identity() -> Result<BasicIdentity, String> {
