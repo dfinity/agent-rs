d6f260e850534291e5d8fbd84d05a3f8497fcd4d
Prithvi Shahi <prithvi.shahi@dfinity.org>
2021-03-24 17:53:42 -0700

    fix

--- ic-utils/src/interfaces/wallet.rs
+++ ic-utils/src/interfaces/wallet.rs
@@ -336,35 +336,6 @@ impl<'agent> Canister<'agent, Wallet> {
         &'canister self,
         wasm_module: Vec<u8>,
     ) -> impl 'agent + AsyncCall<()> {
-        self.update_("wallet_store_wallet_wasm")
-            .with_arg(wasm_module)
-            .build()
-    }
-
-    /// Create a wallet canister
-    pub fn wallet_create_wallet<'canister: 'agent>(
-        &'canister self,
-        cycles: u64,
-        controller: Option<Principal>,
-    ) -> impl 'agent + AsyncCall<(CreateResult,)> {
-        #[derive(CandidType)]
-        struct In {
-            cycles: u64,
-            controller: Option<Principal>,
-        }
-
-        self.update_("wallet_create_wallet")
-            .with_arg(In { cycles, controller })
-            .build()
-            .map(|result: (CreateResult,)| (result.0,))
-    }
-
-    /// Store the wallet WASM inside the wallet canister.
-    /// This is needed to enable wallet_create_wallet
-    pub fn wallet_store_wallet_wasm<'canister: 'agent>(
-        &'canister self,
-        wasm_module: Vec<u8>,
-    ) -> impl 'agent + AsyncCall<()> {
         #[derive(CandidType, Deserialize)]
         struct In {
             #[serde(with = "serde_bytes")]
