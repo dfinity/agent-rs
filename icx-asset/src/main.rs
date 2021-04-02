b35136e887e49f7b20dc9415d7e99ac4c397d01d
Hans Larsen <hans@larsen.online>
2021-04-01 21:02:24 -0700

    .

--- icx-asset/src/main.rs
+++ icx-asset/src/main.rs
@@ -1,10 +1,15 @@
 use clap::{crate_authors, crate_version, AppSettings, Clap};
-use ic_agent::identity::{AnonymousIdentity, BasicIdentity};
+use ic_agent::identity::AnonymousIdentity;
 use ic_agent::{agent, Agent, Identity};
+use ic_types::Principal;
+use ic_utils::Canister;
 use std::collections::HashMap;
+use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use walkdir::WalkDir;
+
+type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[derive(Clap)]
 #[clap(
@@ -54,10 +59,94 @@ fn create_identity(maybe_pem: Option<PathBuf>) -> Box<dyn Identity> {
     // }
 }
 
-fn upload(agent: Agent, opts: &Opts, o: &UploadOpts) -> Result<(), Box<dyn std::error::Error>> {
-    // let canister = ic_utils::Canister::builder().with_agent(&agent ).with_canister_id(Principal::from_text(&opts.canister_id)?).build()?;
+struct ChunkIterator<T>
+where
+    T: Read,
+{
+    pub source: T,
+    pub chunk_size: usize,
+}
 
+struct Chunk(pub Vec<u8>);
+
+impl<T> Iterator for ChunkIterator<T>
+where
+    T: Read,
+{
+    type Item = Chunk;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut buffer = vec![0; self.chunk_size];
+        match self.source.read(buffer.as_mut_slice()) {
+            Ok(count) => {
+                if count > 0 {
+                    buffer.split_off(count);
+                    Some(Chunk(buffer))
+                } else {
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
+}
+
+#[derive(CandidType, Deserialize)]
+struct CreateChunkArg<'a> {
+    pub batch_id: &'a candid::Nat,
+    #[serde(with = "serde_bytes")]
+    pub content: Vec<u8>,
+}
+
+#[derive(CandidType, Deserialize)]
+struct CreateChunkReturn {
+    pub chunk_id: candid::Nat,
+}
+
+/// Upload an iterator of file paths to the asset canister, in a batch. The batch should be created
+/// prior to calling this. This does not apply any logic currently as to whether files should
+/// be uploaded; all files passed in WILL uploaded.
+async fn do_upload(
+    canister: &Canister<'_>,
+    batch_id: candid::Nat,
+    files: impl Iterator<Item = (String, PathBuf)>,
+    chunk_size: usize,
+) -> Result<Vec<(String, Vec<candid::Nat>)>> {
+    // First, split the files into chunks.
+    let files = files
+        .filter_map(|(name, path)| Some((name, std::fs::File::open(path).ok()?)))
+        .flat_map(|(name, file)| {
+            let chunks = ChunkIterator {
+                source: file,
+                chunk_size,
+            };
+
+            // Since no chunk really own the name, we create an Arc that share a reference
+            // to it.
+            let name = std::sync::Arc::new(name);
+
+            chunks.map(move |chunk| (std::sync::Arc::clone(&name), chunk))
+        });
+
+    let result = Vec::new();
+    for (name, chunk) in files {
+        canister
+            .update_("create_chunk")
+            .with_arg(CreateChunkArg {
+                batch_id: &batch_id,
+                content: chunk.0,
+            })
+            .build()
+            .call_and_wait()
+            .await?;
+    }
+
+    Ok(result)
+}
+
+async fn upload(agent: Agent, opts: &Opts, o: &UploadOpts) -> Result {
     let mut key_map: HashMap<String, PathBuf> = HashMap::new();
+
     for arg in &o.files {
         let (key, source): (String, PathBuf) = {
             if let Some(index) = arg.find('=') {
@@ -75,22 +164,30 @@ fn upload(agent: Agent, opts: &Opts, o: &UploadOpts) -> Result<(), Box<dyn std::
         } else {
             for p in WalkDir::new(source.clone())
                 .into_iter()
-                .filter_map(Result::ok)
+                .filter_map(std::result::Result::ok)
                 .filter(|e| !e.file_type().is_dir())
             {
                 let p: &Path = p.path();
-                key_map.insert(key.to_string() + "/" + &p.to_string_lossy(), source.join(p));
+                let key = key.to_string() + "/" + &p.to_string_lossy();
+                let source = p.to_path_buf();
+                key_map.insert(key, source);
             }
         }
     }
 
-    eprintln!("  {:?}", key_map);
+    let canister = ic_utils::Canister::builder()
+        .with_agent(&agent)
+        .with_canister_id(Principal::from_text(&opts.canister_id)?)
+        .build()?;
+
+    eprintln!("{:#?}", key_map);
+    do_upload(&canister, key_map.into_iter(), 1024 * 1024).await?;
 
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() -> Result {
     let opts: Opts = Opts::parse();
 
     let agent = Agent::builder()
@@ -102,7 +199,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match &opts.subcommand {
         SubCommand::Upload(o) => {
-            upload(agent, &opts, o)?;
+            upload(agent, &opts, o).await?;
         }
     }
 
