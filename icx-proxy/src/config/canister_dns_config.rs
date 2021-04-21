1cb6dcffcb0deab223a1dea2f5264fe1a2424083
Eric Swanson <eric.swanson@dfinity.org>
2021-04-21 09:57:39 -0700

    fix: [icx-proxy] Sort aliases from longest to shortest
    Fixes https://github.com/dfinity/agent-rs/issues/153

--- icx-proxy/src/config/canister_dns_config.rs
+++ icx-proxy/src/config/canister_dns_config.rs
@@ -1,5 +1,6 @@
 use crate::config::dns_alias::DnsAlias;
 use ic_types::Principal;
+use std::cmp::Reverse;
 
 /// Configuration for determination of Domain Name to Principal
 #[derive(Clone, Debug)]
@@ -11,10 +12,13 @@ impl CanisterDnsConfig {
     /// Create a CanisterDnsConfig instance from command-line configuration.
     /// dns_aliases: 0 or more entries of the form of dns.alias:canister-id
     pub fn new(dns_aliases: &[String]) -> anyhow::Result<CanisterDnsConfig> {
+        let mut dns_aliases: Vec<String> = dns_aliases.iter().map(String::clone).collect();
+        dns_aliases.sort_by_key(|s| Reverse(s.len()));
         let dns_aliases = dns_aliases
             .iter()
-            .map(|alias| DnsAlias::new(alias))
+            .map(|alias| DnsAlias::new(&alias))
             .collect::<anyhow::Result<Vec<DnsAlias>>>()?;
+
         Ok(CanisterDnsConfig { dns_aliases })
     }
 
@@ -141,6 +145,86 @@ mod tests {
             dns_aliases.resolve_canister_id_from_split_hostname(&["another", "of", "many"]),
             Some(Principal::from_text("rrkah-fqaaa-aaaaa-aaaaq-cai").unwrap())
         )
+    }
+
+    #[test]
+    fn searches_longest_to_shortest() {
+        // If we checked these in the order passed, a.b.c would erroneously resolve
+        // to the canister id associated with b.c
+        let dns_aliases = parse_dns_aliases(vec![
+            "b.c:rrkah-fqaaa-aaaaa-aaaaq-cai",
+            "a.b.c:r7inp-6aaaa-aaaaa-aaabq-cai",
+        ])
+        .unwrap();
+
+        assert_eq!(
+            dns_aliases.resolve_canister_id_from_split_hostname(&["a", "b", "c"]),
+            Some(Principal::from_text("r7inp-6aaaa-aaaaa-aaabq-cai").unwrap())
+        );
+        assert_eq!(
+            dns_aliases.resolve_canister_id_from_split_hostname(&["d", "b", "c"]),
+            Some(Principal::from_text("rrkah-fqaaa-aaaaa-aaaaq-cai").unwrap())
+        );
+    }
+
+    #[test]
+    fn searches_longest_to_shortest_even_if_already_ordered() {
+        // Similar to searches_longest_to_shortest, just to ensure that
+        // we do the right thing no matter which order they are passed.
+        let dns_aliases = parse_dns_aliases(vec![
+            "a.b.c:r7inp-6aaaa-aaaaa-aaabq-cai",
+            "b.c:rrkah-fqaaa-aaaaa-aaaaq-cai",
+        ])
+        .unwrap();
+
+        assert_eq!(
+            dns_aliases.resolve_canister_id_from_split_hostname(&["a", "b", "c"]),
+            Some(Principal::from_text("r7inp-6aaaa-aaaaa-aaabq-cai").unwrap())
+        );
+        assert_eq!(
+            dns_aliases.resolve_canister_id_from_split_hostname(&["d", "b", "c"]),
+            Some(Principal::from_text("rrkah-fqaaa-aaaaa-aaaaq-cai").unwrap())
+        );
+    }
+
+    #[test]
+    fn searches_longest_to_shortest_not_alpha() {
+        // Similar to searches_longest_to_shortest, but make sure we
+        // are sorting by length, not alphabetically
+        let dns_aliases = parse_dns_aliases(vec![
+            "a.x.c:r7inp-6aaaa-aaaaa-aaabq-cai",
+            "x.c:rrkah-fqaaa-aaaaa-aaaaq-cai",
+        ])
+        .unwrap();
+
+        assert_eq!(
+            dns_aliases.resolve_canister_id_from_split_hostname(&["a", "x", "c"]),
+            Some(Principal::from_text("r7inp-6aaaa-aaaaa-aaabq-cai").unwrap())
+        );
+        assert_eq!(
+            dns_aliases.resolve_canister_id_from_split_hostname(&["d", "x", "c"]),
+            Some(Principal::from_text("rrkah-fqaaa-aaaaa-aaaaq-cai").unwrap())
+        );
+    }
+
+    #[test]
+    fn searches_longest_to_shortest_not_alpha_reversed() {
+        // Similar to searches_longest_to_shortest, but make sure we
+        // are sorting by length, not alphabetically
+        let dns_aliases = parse_dns_aliases(vec![
+            "x.a.c:r7inp-6aaaa-aaaaa-aaabq-cai",
+            "a.c:rrkah-fqaaa-aaaaa-aaaaq-cai",
+        ])
+        .unwrap();
+
+        assert_eq!(
+            dns_aliases.resolve_canister_id_from_split_hostname(&["x", "a", "c"]),
+            Some(Principal::from_text("r7inp-6aaaa-aaaaa-aaabq-cai").unwrap())
+        );
+        assert_eq!(
+            dns_aliases.resolve_canister_id_from_split_hostname(&["d", "a", "c"]),
+            Some(Principal::from_text("rrkah-fqaaa-aaaaa-aaaaq-cai").unwrap())
+        );
     }
 
     fn parse_dns_aliases(aliases: Vec<&str>) -> anyhow::Result<CanisterDnsConfig> {
