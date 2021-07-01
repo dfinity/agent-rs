0e0278d2f0c9852784be5a698c2db5554cf8a93d
Eric Swanson <eric.swanson@dfinity.org>
2021-07-01 12:37:52 -0700

    chore: re-add BigUInt, OID imports needed when building for dfx

--- ic-agent/src/identity/secp256k1.rs
+++ ic-agent/src/identity/secp256k1.rs
@@ -12,7 +12,7 @@ use openssl::pkey::{Private, Public};
 use openssl::sha::sha256;
 use simple_asn1::ASN1Block;
 use simple_asn1::ASN1Block::{BitString, ObjectIdentifier, Sequence};
-use simple_asn1::{oid, to_der};
+use simple_asn1::{oid, to_der, BigUint, OID};
 
 #[derive(Clone, Debug)]
 pub struct Secp256k1Identity {
