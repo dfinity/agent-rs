52576f9846ef83f1ee720eade1e40d51790d5c26
Eric Swanson <eric.swanson@dfinity.org>
2021-07-09 15:30:05 -0700

    forgot sync

--- e2e/bash/icx-asset.bash
+++ e2e/bash/icx-asset.bash
@@ -19,7 +19,7 @@ teardown() {
 
 icx_asset_sync() {
   CANISTER_ID=$(dfx canister id e2e_project_assets)
-  $ICX_ASSET --pem $HOME/.config/dfx/identity/default/identity.pem $CANISTER_ID src/e2e_project_assets/assets
+  assert_command $ICX_ASSET --pem $HOME/.config/dfx/identity/default/identity.pem sync $CANISTER_ID src/e2e_project_assets/assets
 }
 
 @test "no-nop test" {
@@ -30,7 +30,7 @@ icx_asset_sync() {
   echo "new file content" >src/e2e_project_assets/assets/new-asset.txt
   icx_asset_sync
 
-  dfx canister --no-wallet call --query e2e_project_assets get '(record{key="/new-asset.txt";accept_encodings=vec{"identity"}})'
+  assert_command dfx canister --no-wallet call --query e2e_project_assets get '(record{key="/new-asset.txt";accept_encodings=vec{"identity"}})'
 }
 
 @test "updates existing files" {
@@ -43,13 +43,13 @@ icx_asset_sync() {
 
 @test "unsets asset encodings that are removed from project" {
 
-    dfx canister --no-wallet call --update e2e_project_assets store '(record{key="/sample-asset.txt"; content_type="text/plain"; content_encoding="arbitrary"; content=blob "content encoded in another way!"})'
+    assert_command dfx canister --no-wallet call --update e2e_project_assets store '(record{key="/sample-asset.txt"; content_type="text/plain"; content_encoding="arbitrary"; content=blob "content encoded in another way!"})'
 
-    dfx canister --no-wallet call --query e2e_project_assets get '(record{key="/sample-asset.txt";accept_encodings=vec{"identity"}})'
-    dfx canister --no-wallet call --query e2e_project_assets get '(record{key="/sample-asset.txt";accept_encodings=vec{"arbitrary"}})'
+    assert_command dfx canister --no-wallet call --query e2e_project_assets get '(record{key="/sample-asset.txt";accept_encodings=vec{"identity"}})'
+    assert_command dfx canister --no-wallet call --query e2e_project_assets get '(record{key="/sample-asset.txt";accept_encodings=vec{"arbitrary"}})'
 
     icx_asset_sync
 
-    dfx canister --no-wallet call --query e2e_project_assets get '(record{key="/sample-asset.txt";accept_encodings=vec{"identity"}})'
+    assert_command dfx canister --no-wallet call --query e2e_project_assets get '(record{key="/sample-asset.txt";accept_encodings=vec{"identity"}})'
     assert_command_fail dfx canister --no-wallet call --query e2e_project_assets get '(record{key="/sample-asset.txt";accept_encodings=vec{"arbitrary"}})'
 }
