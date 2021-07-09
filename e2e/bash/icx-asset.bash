9b38534561e9f9c7da03ae4f6470806bcf4012b9
Eric Swanson <eric.swanson@dfinity.org>
2021-07-09 12:04:58 -0700

    try to use dfx

--- e2e/bash/icx-asset.bash
+++ e2e/bash/icx-asset.bash
@@ -7,10 +7,15 @@ source $BATS_SUPPORT/load.bash
 
 setup() {
     cd $(mktemp -d -t icx-asset-e2e-XXXXXXXX)
+    dfx new e2e_project
+    cd e2e_project
+    dfx start --background
+    dfx deploy
 }
 
 teardown() {
     echo teardown
+    dfx stop
 }
 
 @test "no-nop test" {
