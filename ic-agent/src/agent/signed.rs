7ddb3d2acf53f7c223e9bf03af332c618d45a592
Linwei Shang <linwei.shang@dfinity.org>
2021-07-14 07:57:36 -0700

    Add serde_json tests

--- ic-agent/src/agent/signed.rs
+++ ic-agent/src/agent/signed.rs
@@ -42,3 +42,25 @@ pub struct SignedRequestStatus {
     #[serde(with = "serde_bytes")]
     pub signed_request_status: Vec<u8>,
 }
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_query_serde() {
+        let query = SignedQuery {
+            ingress_expiry: 1,
+            sender: Principal::from_slice(&[0; 29]),
+            canister_id: Principal::from_slice(&[0; 29]),
+            method_name: "greet".to_string(),
+            arg: vec![0,1],
+            effective_canister_id: Principal::from_slice(&[0; 29]),
+            signed_query: vec![0,1,2,3],
+        };
+        let serialized = serde_json::to_string(&query).unwrap();
+        let deserialized = serde_json::from_str::<SignedQuery>(&serialized);
+        assert!(deserialized.is_ok());
+    }
+}
