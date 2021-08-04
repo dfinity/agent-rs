a70f34f5ab5061601790ca5d5c9154c540796956
Eric Swanson <eric.swanson@dfinity.org>
2021-08-04 10:27:40 -0700

    wip

--- e2e/bash/icx-asset-upload.bash
+++ e2e/bash/icx-asset-upload.bash
@@ -6,7 +6,11 @@ source "$BATS_SUPPORT"/load.bash
 load util/assertions
 
 setup() {
-    cd "$(mktemp -d -t icx-asset-e2e-XXXXXXXX)" || exit 1
+      # We want to work from a different temporary directory for every test.
+    x=$(mktemp -d -t icx-asset-e2e-XXXXXXXX)
+    export DFX_CONFIG_ROOT="$x"
+    cd "$DFX_CONFIG_ROOT" || exit
+
     dfx new --no-frontend e2e_project
     cd e2e_project || exit 1
     dfx start --background
@@ -14,23 +18,23 @@ setup() {
 }
 
 teardown() {
-    echo teardown
     dfx stop
+    rm -rf "$DFX_CONFIG_ROOT"
 }
 
 icx_asset_sync() {
   CANISTER_ID=$(dfx canister id e2e_project_assets)
-  assert_command "$ICX_ASSET" --pem "$HOME"/.config/dfx/identity/default/identity.pem sync "$CANISTER_ID" src/e2e_project_assets/assets
+  assert_command "$ICX_ASSET" --pem "$DFX_CONFIG_ROOT"/.config/dfx/identity/default/identity.pem sync "$CANISTER_ID" src/e2e_project_assets/assets
 }
 
 icx_asset_list() {
   CANISTER_ID=$(dfx canister id e2e_project_assets)
-  assert_command "$ICX_ASSET" --pem "$HOME"/.config/dfx/identity/default/identity.pem ls "$CANISTER_ID"
+  assert_command "$ICX_ASSET" --pem "$DFX_CONFIG_ROOT"/.config/dfx/identity/default/identity.pem ls "$CANISTER_ID"
 }
 
 icx_asset_upload() {
   CANISTER_ID=$(dfx canister id e2e_project_assets)
-  assert_command "$ICX_ASSET" --pem "$HOME"/.config/dfx/identity/default/identity.pem upload "$CANISTER_ID" "$*"
+  assert_command "$ICX_ASSET" --pem "$DFX_CONFIG_ROOT"/.config/dfx/identity/default/identity.pem upload "$CANISTER_ID" "$*"
 }
 
 @test "uploads a file by name" {
@@ -56,7 +60,7 @@ icx_asset_upload() {
     #   /some_dir/a.txt
     #   /some_dir/b.txt
 
-    assert_match " /uploaded.txt.*text/plain.*identity"
+    assert_match " /some_dir/a.txt.*text/plain.*identity"
 }
 
 @test "uploads multiple files" {
