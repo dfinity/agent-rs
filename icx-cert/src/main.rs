c712fdfe4edc7f34a0140dba129025f6d58ffd06
Eric Swanson <64809312+ericswanson-dfinity@users.noreply.github.com>
2021-10-21 10:55:11 -0700

    feat: icx-cert: add --accept-encoding parameter, display content-type response header value if present (#276)

--- icx-cert/src/main.rs
+++ icx-cert/src/main.rs
@@ -11,11 +11,20 @@ mod pprint;
 enum Command {
     /// Fetches the specified URL and pretty-prints the certificate.
     #[clap(name = "print")]
-    PPrint { url: String },
+    PPrint {
+        url: String,
+
+        /// Specifies one or more encodings to accept.
+        #[clap(long, multiple(true), number_of_values(1))]
+        accept_encoding: Option<Vec<String>>,
+    },
 }
 
 fn main() -> Result<()> {
     match Command::parse() {
-        Command::PPrint { url } => pprint::pprint(url),
+        Command::PPrint {
+            url,
+            accept_encoding,
+        } => pprint::pprint(url, accept_encoding),
     }
 }
