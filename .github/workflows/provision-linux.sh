d4149cf6c3477c549a1720f1489159a55ecd4490
Eric Swanson <eric.swanson@dfinity.org>
2022-02-08 10:06:45 -0800

    chore: Use dfx 0.8.4 for setup for e2e tests

--- .github/workflows/provision-linux.sh
+++ .github/workflows/provision-linux.sh
@@ -22,7 +22,7 @@ sudo tar --directory /usr/local/lib/bats-support --extract --file v$version.tar.
 rm v$version.tar.gz
 
 # Install DFINITY SDK.
-version=0.7.2
+version=0.8.4
 wget --output-document install-dfx.sh "https://sdk.dfinity.org/install.sh"
 DFX_VERSION=$version bash install-dfx.sh < <(yes Y)
 rm install-dfx.sh
