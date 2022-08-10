37605b66dc4ec5b2a3f606d74dedc082cc074614
Daniel.Bloom <daniel.bloom@dfinity.org>
2022-08-09 19:15:26 -0700

    lints and test fixes

--- ic-agent/src/agent/http_transport/mod.rs
+++ ic-agent/src/agent/http_transport/mod.rs
@@ -12,5 +12,7 @@ pub mod hyper_transport;
 #[cfg(feature = "hyper")]
 pub use hyper_transport::*;
 
+#[allow(dead_code)]
 const IC0_DOMAIN: &str = "ic0.app";
+#[allow(dead_code)]
 const IC0_SUB_DOMAIN: &str = ".ic0.app";
