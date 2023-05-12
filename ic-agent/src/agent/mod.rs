92228877d4dfd5375a8f3c894170c9bc7d07108d
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2023-05-12 12:38:21 +0000

    .

--- ic-agent/src/agent/mod.rs
+++ ic-agent/src/agent/mod.rs
@@ -270,6 +270,23 @@ impl Agent {
         })
     }
 
+    /// Set the transport of the [`Agent`].
+    pub fn set_transport<F: 'static + Transport>(&mut self, transport: F) {
+        self.transport = Arc::new(transport);
+    }
+
+    /// Set the identity provider for signing messages.
+    ///
+    /// NOTE: if you change the identity while having update calls in
+    /// flight, you will not be able to [Agent::poll] the status of these
+    /// messages.
+    pub fn set_identity<I>(&mut self, identity: I)
+    where
+        I: 'static + Identity,
+    {
+        self.identity = Arc::new(identity);
+    }
+
     /// By default, the agent is configured to talk to the main Internet Computer, and verifies
     /// responses using a hard-coded public key.
     ///
