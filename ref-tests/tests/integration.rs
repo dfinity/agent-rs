a3cb096aaa120cef4987a99c603bdc6f5350c85a
Eric Swanson <eric.swanson@dfinity.org>
2023-09-27 14:11:35 -0700

    wow

--- ref-tests/tests/integration.rs
+++ ref-tests/tests/integration.rs
@@ -104,7 +104,7 @@ fn canister_reject_call() {
             result,
             Err(AgentError::ReplicaError(RejectResponse {
                 reject_code: RejectCode::DestinationInvalid,
-                reject_message: "method does not exist: wallet_send".to_string(),
+                reject_message: format!("Canister {} has no update method 'wallet_send'", alice.canister_id()),
                 error_code: None
             }))
         );
