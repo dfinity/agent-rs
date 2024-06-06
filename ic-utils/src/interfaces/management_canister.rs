be929fd7967249c879f48f2f494cbfc5805a7d98
Adam Spofford <93943719+adamspofford-dfinity@users.noreply.github.com>
2024-06-06 10:29:14 -0700

    feat: Move Bitcoin methods to new BitcoinCanister (#567)

--- ic-utils/src/interfaces/management_canister.rs
+++ ic-utils/src/interfaces/management_canister.rs
@@ -73,17 +73,13 @@ pub enum MgmtMethod {
     EcdsaPublicKey,
     /// There is no corresponding agent function as only canisters can call it.
     SignWithEcdsa,
-    /// There is no corresponding agent function as only canisters can call it.
+    /// There is no corresponding agent function as only canisters can call it. Use [`BitcoinCanister`](super::BitcoinCanister) instead.
     BitcoinGetBalance,
-    /// See [`ManagementCanister::bitcoin_get_balance_query`].
-    BitcoinGetBalanceQuery,
-    /// There is no corresponding agent function as only canisters can call it.
+    /// There is no corresponding agent function as only canisters can call it. Use [`BitcoinCanister`](super::BitcoinCanister) instead.
     BitcoinGetUtxos,
-    /// See [`ManagementCanister::bitcoin_get_utxos_query`].
-    BitcoinGetUtxosQuery,
-    /// There is no corresponding agent function as only canisters can call it.
+    /// There is no corresponding agent function as only canisters can call it. Use [`BitcoinCanister`](super::BitcoinCanister) instead.
     BitcoinSendTransaction,
-    /// There is no corresponding agent function as only canisters can call it.
+    /// There is no corresponding agent function as only canisters can call it. Use [`BitcoinCanister`](super::BitcoinCanister) instead.
     BitcoinGetCurrentFeePercentiles,
     /// There is no corresponding agent function as only canisters can call it.
     NodeMetricsHistory,
@@ -234,67 +230,6 @@ pub type StoreChunksResult = Vec<ChunkHash>;
 
 /// Return type of [ManagementCanister::upload_chunk].
 pub type UploadChunkResult = ChunkHash;
-
-/// The Bitcoin network that a Bitcoin transaction is placed on.
-#[derive(Clone, Copy, Debug, CandidType, Deserialize, PartialEq, Eq)]
-pub enum BitcoinNetwork {
-    /// The BTC network.
-    #[serde(rename = "mainnet")]
-    Mainnet,
-    /// The TESTBTC network.
-    #[serde(rename = "testnet")]
-    Testnet,
-    /// The REGTEST network.
-    ///
-    /// This is only available when developing with local replica.
-    #[serde(rename = "regtest")]
-    Regtest,
-}
-
-/// Defines how to filter results from [`bitcoin_get_utxos_query`](ManagementCanister::bitcoin_get_utxos_query).
-#[derive(Debug, Clone, CandidType, Deserialize)]
-pub enum UtxosFilter {
-    /// Filter by the minimum number of UTXO confirmations. Most applications should set this to 6.
-    #[serde(rename = "min_confirmations")]
-    MinConfirmations(u32),
-    /// When paginating results, use this page. Provided by [`GetUtxosResponse.next_page`](GetUtxosResponse).
-    #[serde(rename = "page")]
-    Page(#[serde(with = "serde_bytes")] Vec<u8>),
-}
-
-/// Unique output descriptor of a Bitcoin transaction.
-#[derive(Debug, Clone, CandidType, Deserialize)]
-pub struct UtxoOutpoint {
-    /// The ID of the transaction. Not necessarily unique on its own.
-    pub txid: Vec<u8>,
-    /// The index of the outpoint within the transaction.
-    pub vout: u32,
-}
-
-/// A Bitcoin [`UTXO`](https://en.wikipedia.org/wiki/Unspent_transaction_output), produced by a transaction.
-#[derive(Debug, Clone, CandidType, Deserialize)]
-pub struct Utxo {
-    /// The transaction outpoint that produced this UTXO.
-    pub outpoint: UtxoOutpoint,
-    /// The BTC quantity, in satoshis.
-    pub value: u64,
-    /// The block index this transaction was placed at.
-    pub height: u32,
-}
-
-/// Response type for the `bitcoin_get_utxos_query` function.
-#[derive(Debug, Clone, CandidType, Deserialize)]
-pub struct GetUtxosResponse {
-    /// A list of UTXOs available for the specified address.
-    pub utxos: Vec<Utxo>,
-    /// The hash of the tip.
-    pub tip_block_hash: Vec<u8>,
-    /// The block index of the tip of the chain known to the IC.
-    pub tip_height: u32,
-    /// If `Some`, then `utxos` does not contain the entire results of the query.
-    /// Call `bitcoin_get_utxos_query` again using `UtxosFilter::Page` for the next page of results.
-    pub next_page: Option<Vec<u8>>,
-}
 
 impl<'agent> ManagementCanister<'agent> {
     /// Get the status of a canister.
@@ -543,57 +478,6 @@ impl<'agent> ManagementCanister<'agent> {
                 canister_id: *canister_id,
             })
             .with_effective_canister_id(*canister_id)
-            .build()
-    }
-
-    /// Gets the BTC balance (in satoshis) of a particular Bitcoin address, filtering by number of confirmations.
-    /// Most applications should require 6 confirmations.
-    pub fn bitcoin_get_balance_query(
-        &self,
-        address: &str,
-        network: BitcoinNetwork,
-        min_confirmations: Option<u32>,
-    ) -> impl 'agent + SyncCall<Value = (u64,)> {
-        #[derive(CandidType)]
-        struct In<'a> {
-            address: &'a str,
-            network: BitcoinNetwork,
-            min_confirmations: Option<u32>,
-        }
-        self.query(MgmtMethod::BitcoinGetBalanceQuery.as_ref())
-            .with_arg(In {
-                address,
-                network,
-                min_confirmations,
-            })
-            .with_effective_canister_id(Principal::management_canister())
-            .build()
-    }
-
-    /// Fetch the list of [UTXOs](https://en.wikipedia.org/wiki/Unspent_transaction_output) for a Bitcoin address,
-    /// filtering by number of confirmations. Most applications should require 6 confirmations.
-    ///
-    /// This method is paginated. If not all the results can be returned, then `next_page` will be set to `Some`,
-    /// and its value can be passed to this method to get the next page.
-    pub fn bitcoin_get_utxos_query(
-        &self,
-        address: &str,
-        network: BitcoinNetwork,
-        filter: Option<UtxosFilter>,
-    ) -> impl 'agent + SyncCall<Value = (GetUtxosResponse,)> {
-        #[derive(CandidType)]
-        struct In<'a> {
-            address: &'a str,
-            network: BitcoinNetwork,
-            filter: Option<UtxosFilter>,
-        }
-        self.query(MgmtMethod::BitcoinGetUtxosQuery.as_ref())
-            .with_arg(In {
-                address,
-                network,
-                filter,
-            })
-            .with_effective_canister_id(Principal::management_canister())
             .build()
     }
 }
