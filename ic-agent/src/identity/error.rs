1a945c062b90769a10761ffc9c3d3e024d06a283
Adam Spofford <adam.spofford@dfinity.org>
2024-10-03 08:31:42 -0700

    Check delegation in DelegatedIdentity

--- ic-agent/src/identity/error.rs
+++ ic-agent/src/identity/error.rs
@@ -1,3 +1,4 @@
+use ic_transport_types::Delegation;
 use thiserror::Error;
 
 /// An error happened while reading a PEM file.
@@ -23,4 +24,24 @@ pub enum PemError {
     /// The key was rejected by k256.
     #[error("A key was rejected by k256: {0}")]
     ErrorStack(#[from] k256::pkcs8::Error),
+}
+
+/// An error occurred constructing a [`DelegatedIdentity`](super::delegated::DelegatedIdentity).
+#[derive(Error, Debug)]
+pub enum DelegationError {
+    /// Parsing error in delegation bytes.
+    #[error("A delegation could not be parsed")]
+    Parse,
+    /// A key in the chain did not match the signature of the next chain link. If `to` is `None` then it refers to the `Identity`.
+    #[error("A link was missing in the delegation chain")]
+    BrokenChain {
+        from: Vec<u8>,
+        to: Option<Delegation>,
+    },
+    /// A key with an unknown algorithm was used. The IC supports Ed25519, secp256k1, and prime256v1, and in ECDSA the curve must be specified.
+    #[error("The delegation chain contained a key with an unknown algorithm")]
+    UnknownAlgorithm,
+    /// One of `Identity`'s functions returned an error.
+    #[error("A delegated-to identity encountered an error: {0}")]
+    IdentityError(String),
 }
