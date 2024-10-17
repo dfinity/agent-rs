e56d09270f99960057988e52b747911aeddfb7f7
Adam Spofford <adam.spofford@dfinity.org>
2024-10-17 13:46:58 -0700

    .

--- ref-tests/tests/integration.rs
+++ ref-tests/tests/integration.rs
@@ -260,8 +260,8 @@ fn wallet_canister_create_and_install() {
 
         let args = Argument::from_candid((install_config,));
 
-        let _: () = wallet
-            .call64(Principal::management_canister(), "install_code", args, 0)
+        wallet
+            .call64::<(), _>(Principal::management_canister(), "install_code", args, 0)
             .call_and_wait()
             .await?;
 
