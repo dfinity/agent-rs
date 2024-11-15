658d4b6bad8134e7a9cde46349c703f95f75328a
Martin Raszyk <martin.raszyk@dfinity.org>
2024-11-15 16:54:17 +0100

    chore: remove unused e2e tests

--- .github/workflows/provision-darwin.sh
+++ .github/workflows/provision-darwin.sh
@@ -11,23 +11,6 @@ curl --location --output node.pkg "https://nodejs.org/dist/v$version/node-v$vers
 sudo installer -pkg node.pkg -store -target /
 rm node.pkg
 
-# Install Bats.
-if [ "$(uname -r)" = "19.6.0" ]; then
-    brew unlink bats
-fi
-brew install bats-core
-
-# Install Bats support.
-version=0.3.0
-curl --location --output bats-support.tar.gz https://github.com/ztombol/bats-support/archive/v$version.tar.gz
-mkdir /usr/local/lib/bats-support
-tar --directory /usr/local/lib/bats-support --extract --file bats-support.tar.gz --strip-components 1
-rm bats-support.tar.gz
-
-# Set environment variables.
-BATS_SUPPORT="/usr/local/lib/bats-support"
-echo "BATS_SUPPORT=${BATS_SUPPORT}" >> "$GITHUB_ENV"
-
 # Exit temporary directory.
 popd
 
--- .github/workflows/provision-linux.sh
+++ .github/workflows/provision-linux.sh
@@ -11,21 +11,6 @@ sudo bash install-node.sh
 sudo apt-get install --yes nodejs
 rm install-node.sh
 
-# Install Bats.
-sudo apt-get install --yes bats
-
-# Install Bats support.
-version=0.3.0
-curl --location --output bats-support.tar.gz https://github.com/ztombol/bats-support/archive/v$version.tar.gz
-sudo mkdir /usr/local/lib/bats-support
-sudo tar --directory /usr/local/lib/bats-support --extract --file bats-support.tar.gz --strip-components 1
-rm bats-support.tar.gz
-
-# Set environment variables.
-BATS_SUPPORT="/usr/local/lib/bats-support"
-echo "BATS_SUPPORT=${BATS_SUPPORT}" >> "$GITHUB_ENV"
-echo "$HOME/bin" >> "$GITHUB_PATH"
-
 # Exit temporary directory.
 popd
 
